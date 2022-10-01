4a324bd14ebbb9e5b1a9f50b5c235e3de4d08195
ice <ice@xicex.fr>
2022-10-01 19:33:05 +0200

    add helmet et morgan

--- api/app.js
+++ api/app.js
@@ -2,11 +2,20 @@ const express = require('express');
 const routing = require('./routes/routedispatch');
 require('./database/connect');
 const path = require('path');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const fs = require('fs');
+const accessLogStream = fs.createWriteStream( path.join(__dirname, 'access.log'), {flags: 'a'} ); 
 
 const app = express();
 
+// Log request and response and setup the logger app.use(morgan('combined', {stream: accessLogStream})); 
+app.use(morgan('dev', {stream: accessLogStream}));
+
 app.use(express.json());
 app.use('/images', express.static(path.join(__dirname,'images')));
+
+app.use(helmet());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
